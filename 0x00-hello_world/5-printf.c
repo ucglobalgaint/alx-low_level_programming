7bca7bf81b412eb36038cf470ddff9b0d8ed3de5
ucglobalgaint <uchidozie9@gmail.com>
2022-10-13 08:16:29 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - Entry
  * Return: Always 0
  */
-init main(void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
