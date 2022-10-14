95feace7b4eab1552f541e4f775b2290697d73b4
ucglobalgaint <uchidozie9@gmail.com>
2022-10-14 06:55:20 -0700

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
-#include <stdiop.h>
-/** main - Entry
-  * Return: Always 0
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
@@ -9,5 +10,5 @@ int main(void)
 	printf("size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("size of a float: %i byte(s)\n", sizeof(float));
-	return (0)
+	return (0);
 }
