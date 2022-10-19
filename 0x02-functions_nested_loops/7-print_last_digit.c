60194682d31b72f76be2f206a284a7b36caed75b
ucglobalgaint <uchidozie9@gmail.com>
2022-10-19 01:09:04 -0700

    Task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,10 @@
  */
 int print_last_digit(int n)
 {
-	int x
+	int last = n % 10;
 
 	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+		last = last * -1;
+	_putchar(last + '0');
+	return (last);
 }
