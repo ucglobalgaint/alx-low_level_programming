9216dd1fc57a615f10cb78ed2bf22892645c35f2
ucglobalgaint <uchidozie9@gmail.com>
2022-10-19 03:18:09 -0700

    Task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,10 +21,10 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) * '0');
+				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) * '0');
 		}
-_putchar('\n');
+		_putchar('\n');
 	}
 }
