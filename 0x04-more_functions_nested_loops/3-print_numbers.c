b306744834cb60b8bfb07c9265f7e4e3e409070b
ucglobalgaint <uchidozie9@gmail.com>
2022-10-21 15:02:21 -0700

    Task03

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,18 @@
 
 /**
  * print_numbers - print from 0 to 9
+ *
  * Return: Always 0
  */
 void print_numbers(void)
 {
-	char c;
+	char n = 0;
 
-	for (c = '0'; c <= 9; c++)
+	while (n <= 9)
 	{
-		_putchar(c + '0');
+		_putchar('0' + n);
+n++;
+
 	}
 	_putchar ('\n');
 }
