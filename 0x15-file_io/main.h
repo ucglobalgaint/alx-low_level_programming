055d8fdaf8a63a0dfe9c372ba19996ba28f188c2
ucglobalgaint <uchidozie9@gmail.com>
2022-12-05 06:23:02 -0800

    Task0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,18 +15,4 @@ int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int _strlen(char *str);
 
-/**
- * ELF header functions
-*/
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
-
 #endif /*MAIN_H */
